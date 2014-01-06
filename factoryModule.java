1927ce8da568a7e52fee8c8fd99eb123f102e8f8
Oscar_Mangard <oscarmangard@gmail.com>
2014-01-06 17:01:11 +0100

    Fixed painting for modules

---
+++ factoryModule.java
@@ -0,0 +1,12 @@
+public abstract class factoryModule{
+	public Module createModule(String choice){
+		switch(choice){
+			case "hull": 
+				return 
+				break;
+			case "engine":
+				return 
+				break;
+		}
+	}
+}
